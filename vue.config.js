4f9db4ea4ea240c6aa3f0dc29fa1accfc4722c7c
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-09-02 20:24:36 +0300

    Update vue.config.js name

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
+    name: "Instagram",
     themeColor: "#000000"
   }
 })
