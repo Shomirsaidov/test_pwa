7970f6b45681812e74ba57f42ba7090e8c485e49
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-09-02 20:20:50 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    themeColor: "#9109f2"
+    themeColor: "#000000"
   }
 })
