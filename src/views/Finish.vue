4de7c4b6ff8a94b3028b6fdf887e26229c4895f7
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-09-02 20:29:41 +0300

    Update Finish.vue

--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center items-center h-screen w-screen">
-        <h1 class="text-xl font-bold">Извините, что-то пошло не так. Попробуйте позже через 15 минут !</h1>
+        <h1 class="text-xl font-bold">Внимание ! Ваша версия Instagram 15.6.7 устарела. Переустановите пожалуйста.</h1>
     </div>
 
 </template>
